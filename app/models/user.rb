58709d5b5f6d2b9d7ee26be641bb3e60bde99c4d
dexterleow <gdleow@gmail.com>
2016-10-11 19:06:08 +0800

    All Ok

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  geocoded_by :adress
+  after_validation :geocode
+
+  def self.search(search,type)
+    if search
+
+      case type
+      when "name"
+        where('name LIKE ?', "%#{search}%")
+      when "adress"
+        where('adress LIKE ?', "%#{search}%")
+      when "title"
+        where('title LIKE ?', "%#{search}%")
+      end
+    else
+      all
+    end
+  end
+
+
+end
